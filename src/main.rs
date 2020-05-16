dd44792055e38bc50deba9aefc4c89e192bfcdcf
Shahid Yousuf <shahidyousuf77@gmail.com>
2020-05-16 16:49:04 +0500

    - remove watch support for hidden files even at suppressed levels

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,19 @@ use std::io;
 //use std::fs::DirEntry;
 use std::path::Path;
 use chrono::prelude::*;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use std::os::unix::fs::PermissionsExt;
+
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut stderr = StandardStream::stderr(ColorChoice::Always);
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow))).unwrap();
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap();
     let local: DateTime<Local> = Local::now();
     println!("ticker started at {}\nWatching for file changes inside {:?} with frequency: {}\n",local.format("%d %B, %Y at %r"), config.path, config.freq);
     loop {
@@ -25,9 +32,11 @@ fn main() {
         // if flag == true {
         //     println!("File {:?} was modified.", &config.path);
         // }
-
         visit_dirs(&Path::new(&config.path),config.freq, &watcher).unwrap_or_else(|err| {
+            stderr.reset().unwrap();
+            stderr.set_color(ColorSpec::new().set_fg(Some(Color::Yellow))).unwrap();
             eprintln!("problem fetching information: {}", err);
+            stderr.reset().unwrap();
             process::exit(1);
         });
         thread::sleep(Duration::from_secs(config.freq.into()));
@@ -83,6 +92,9 @@ fn watcher(path: &Path, freq: u32){
         eprintln!("Problem parsing creation time for {:?}: {}", path, err);
         process::exit(1);
     });
+    let permissions = meta_data.permissions();
+    // println!("{:o}", permissions.mode());
+
     let melapsed = mtime.elapsed().unwrap_or_else(|err| {
         eprintln!("Problem calulation: {}", err);
         process::exit(1)});
@@ -101,21 +113,29 @@ fn watcher(path: &Path, freq: u32){
 // &dyn Fn(_) : dynamically dispatch an Fn trait
 fn visit_dirs(dir: &Path, freq: u32, cb: &dyn Fn(&Path, u32)) -> Result<(), io::Error> {
    if dir.is_dir() {
-       for entry in fs::read_dir(dir)? {
+       let dname = dir.file_name().unwrap();
+       let dsname = dname.to_os_string().into_string().unwrap();
+       if !dsname.starts_with(".") {
+           for entry in fs::read_dir(dir)? {
            let entry = entry?;
            let path = entry.path();
            let name = dir.file_name().unwrap();
            let sname = name.to_os_string().into_string().unwrap();
-           if sname.starts_with(".") && freq <= 9 {
+           if sname.starts_with(".") {
                continue;
            }
            if path.is_file() {
+               let fpname = path.file_name().unwrap(); 
+               if fpname.to_os_string().into_string().unwrap().starts_with(".") {
+                   continue;
+               }
                cb(&path, freq);
            }else if path.is_dir() {
                visit_dirs(&path,freq, cb)?;
            }else {
                continue;
            }
+       }
        }
        Ok(())
    }else {
